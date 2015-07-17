6efaad3cb1deee25f20c257ce37f2807ef06dca4
Stanley Blackman <ufcstan@gmail.com>
2015-07-17 01:57:10 -0500

    Customizing Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,11 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+
+
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-// require_tree .
+//= require_tree .
