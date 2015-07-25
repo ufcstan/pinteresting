df2b2b1df13024031540e71bd101ba4e780dd646
Stanley Blackman <ufcstan@gmail.com>
2015-07-25 16:45:37 -0500

    Added pagination features and Pin limit display.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
 
   
   def index
-    @pins = Pin.all.order("created_at DESC").limit(4)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], per_page: 10 )
   end
 
   
