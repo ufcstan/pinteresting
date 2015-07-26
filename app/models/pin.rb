d02fe900bd5e18f7851bf1529354db01b14347f5
Stanley Blackman <ufcstan@gmail.com>
2015-07-26 08:34:19 -0500

    Add names to users and validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,6 +5,8 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
+   validates :name, presence: true
+
 end
 
 
